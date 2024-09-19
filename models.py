0649ffef7e5fb973a6a0379d8e589a279c6bff82
Jay <joaomarcelo1015ja@gmail.com>
2024-09-19 18:25:02 -0300

    classe de usuario

---
+++ models.py
@@ -0,0 +1,53 @@
+class User:
+    def __init__(self, nome, conta, senha, saldo):
+        self.nome = nome
+        self.conta = conta
+        self.__senha = senha
+        self.__saldo = saldo
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def get_conta(self):
+        return self.conta
+
+    # --- Métodos de senha ---
+    def set_senha(self, nova_senha):
+        if len(nova_senha) == 6:
+            if nova_senha.isdigit():
+                self.__senha = nova_senha
+            raise ValueError("A senha contém caracteres inválidos.")
+
+        raise ValueError("A senha deve conter 6 números.")
+
+    def autenticar(self, senha):
+        return self.__senha == senha
+
+    # --- Métodos de saldo ---
+    def get_saldo(self):
+        return self.__saldo
+
+    def add_saldo(self, add_saldo):
+        if type(add_saldo) == float:
+            if add_saldo > 0:
+                self.__saldo += add_saldo
+            elif add_saldo == 0:
+                raise ValueError("O valor não pode ser 0.")
+
+            raise ValueError("O valor deve ser um número positivo.")
+
+        raise ValueError("O valor não é um número.")
+
+    def dec_saldo(self, dec_saldo):
+        if type(dec_saldo) == float:
+            if dec_saldo > 0:
+                self.__saldo -= dec_saldo
+            elif dec_saldo == 0:
+                raise ValueError("O valor não pode ser 0.")
+
+            raise ValueError("O valor deve ser um número positivo.")
+
+        raise ValueError("O valor não é um número.")
